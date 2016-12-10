ba647178cea33eaf8d6137f31d070ed16e53a0b4
ecenurozturk <ecenurrozturk@gmail.com>
2016-12-10 12:07:40 +0200

    bir kac isimlendirme değistirdim

--- db/migrate/20161210095213_create_posts.rb
+++ db/migrate/20161210095213_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
       t.string :header
-      t.text :text
+      t.body :body
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161210095213) do
-
-  create_table "posts", force: :cascade do |t|
-    t.string   "header"
-    t.text     "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
